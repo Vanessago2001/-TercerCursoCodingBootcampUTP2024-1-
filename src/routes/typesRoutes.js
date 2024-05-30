f661a69ed64f9ec493a0785fcb2604b612641ff1
Vanessago2001 <166752546+Vanessago2001@users.noreply.github.com>
2024-05-30 08:12:07 -0500

    Add files via upload

---
+++ src/routes/typesRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { createType, getAllTypes, deleteType, updateType } from "../controllers/typesController.js";
+
+const router = express.Router();
+
+router.post("/", createType);
+router.get("/", getAllTypes);
+router.delete("/:id", deleteType)
+router.put("/:id", updateType )
+
+export default router;
